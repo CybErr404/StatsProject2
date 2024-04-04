7387f0172f48bfd9865a3a7c11937d6cfc20e5ee
CybErr404 <wattsm6@go.stockton.edu>
2024-04-04 15:22:41 -0400

    Added second formula

--- src/Code/CSVDataSalter/Plotter.java
+++ src/Code/CSVDataSalter/Plotter.java
@@ -6,10 +6,10 @@ import java.util.ArrayList;
 
 public class Plotter {
     ArrayList<Double> resultArray = new ArrayList<>();
-    public void plotter(double start, double end) throws FileNotFoundException {
-        PrintWriter writer = new PrintWriter("data.csv");
+    public void plotter1(double start, double end) throws FileNotFoundException {
+        PrintWriter writer = new PrintWriter("data1.csv");
         for(int i = (int) start; i <= end; i++) {
-            double result = formula(i);
+            double result = formula1(i);
             resultArray.add(result);
         }
         for (Double number : resultArray) {
@@ -18,7 +18,21 @@ public class Plotter {
         writer.close();
     }
 
-    public double formula(double increment) {
+    public void plotter2(double start, double end) throws FileNotFoundException {
+        PrintWriter writer = new PrintWriter("data2.csv");
+        for(int i = (int) start; i <= end; i++) {
+            double result = formula2(i);
+            resultArray.add(result);
+        }
+        for (Double number : resultArray) {
+            writer.println(number);
+        }
+        writer.close();
+    }
+
+    public double formula1(double increment) {
         return Math.pow(increment, 2) + (3 * increment) + 5;
     }
+
+    public double formula2(double increment) { return Math.sin(increment); }
 }
--- src/Code/CSVDataSalter/TestPlotter.java
+++ src/Code/CSVDataSalter/TestPlotter.java
@@ -4,7 +4,9 @@ import java.io.FileNotFoundException;
 
 public class TestPlotter {
     public static void main(String[] args) throws FileNotFoundException {
-        Plotter test = new Plotter();
-        test.plotter(1, 10);
+        Plotter testFormula1 = new Plotter();
+        Plotter testFormula2 = new Plotter();
+        testFormula1.plotter1(1, 10);
+        testFormula2.plotter2(1, 10);
     }
 }
