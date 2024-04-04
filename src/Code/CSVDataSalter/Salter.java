93f1e9c91111d86afe2af3d308d782355924b5a7
CybErr404 <wattsm6@go.stockton.edu>
2024-04-04 15:35:19 -0400

    Finished simple salter

--- src/Code/CSVDataSalter/Salter.java
+++ src/Code/CSVDataSalter/Salter.java
@@ -1,4 +1,60 @@
 package Code.CSVDataSalter;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Salter {
+    ArrayList<Double> resultArray = new ArrayList<>();
+    ArrayList<Double> saltArray = new ArrayList<>();
+    public void salter1(double start, double end) throws FileNotFoundException {
+        PrintWriter writer = new PrintWriter("salt1.csv");
+        for(int i = (int) start; i <= end; i++) {
+            double result = formula1(i);
+            resultArray.add(result);
+        }
+        for(int i = 0; i < resultArray.size(); i++) {
+            Random randomNumber = new Random();
+            int randomSaltValue = randomNumber.nextInt(50);
+            if(i % 2 == 0) {
+                saltArray.add(resultArray.get(i) + randomSaltValue);
+            }
+            else {
+                saltArray.add(resultArray.get(i) - randomSaltValue);
+            }
+        }
+        for (Double number : saltArray) {
+            writer.println(number);
+        }
+        writer.close();
+    }
+
+    public void salter2(double start, double end) throws FileNotFoundException {
+        PrintWriter writer = new PrintWriter("salt2.csv");
+        for(int i = (int) start; i <= end; i++) {
+            double result = formula2(i);
+            resultArray.add(result);
+        }
+        for(int i = 0; i < resultArray.size(); i++) {
+            Random randomNumber = new Random();
+            int randomSaltValue = randomNumber.nextInt(50);
+            if(i % 2 == 0) {
+                saltArray.add(resultArray.get(i) + randomSaltValue);
+            }
+            else {
+                saltArray.add(resultArray.get(i) - randomSaltValue);
+            }
+        }
+        for (Double number : saltArray) {
+            writer.println(number);
+        }
+        writer.close();
+    }
+
+    public double formula1(double increment) {
+        return Math.pow(increment, 2) + (3 * increment) + 5;
+    }
+
+    public double formula2(double increment) { return Math.sin(increment); }
 }
--- src/Code/CSVDataSalter/TestSalter.java
+++ src/Code/CSVDataSalter/TestSalter.java
@@ -1,7 +1,12 @@
 package Code.CSVDataSalter;
 
-public class TestSalter {
-    public static void main(String[] arg) {
+import java.io.FileNotFoundException;
 
+public class TestSalter {
+    public static void main(String[] args) throws FileNotFoundException {
+        Salter testFormula1 = new Salter();
+        Salter testFormula2 = new Salter();
+        testFormula1.salter1(1, 10);
+        testFormula2.salter2(1, 10);
     }
 }
