437fa885568e240588e9498534d3eed702c234a2
CybErr404 <wattsm6@go.stockton.edu>
2024-04-17 12:06:03 -0400

    Added comments

--- src/Project2Part1Java/CSVDataSalter/TestSalter.java
+++ src/Project2Part1Java/CSVDataSalter/TestSalter.java
@@ -1,16 +1,26 @@
 package Project2Part1Java.CSVDataSalter;
 
-import Project2Part1Java.CSVDataPlotter.Plotter;
-
+//Imports FileNotFoundExceptions to avoid issues with writing to files.
 import java.io.FileNotFoundException;
 
+/**
+ * @author Mia Watts
+ * This is the tester class for the Java Data Salter. Objects are made and are then used to test
+ * the salting methods, running each formula from its start point, -100, to its end point, 100.
+ */
 public class TestSalter {
     public static void main(String[] args) throws FileNotFoundException {
+        //Creates a salter object for the polynomial formula.
         Salter polynomialFormulaSalt = new Salter();
+        //Creates a salter object for the sine formula.
         Salter sineFormulaSalt = new Salter();
+        //Creates a salter object for the cosine formula.
         Salter cosineFormulaSalt = new Salter();
+        //Writes values calculated from salting the polynomial results from -100 to 100.
         polynomialFormulaSalt.polynomialSalter(-100, 100);
+        //Writes values calculated from salting the sine results from -100 to 100.
         sineFormulaSalt.sineSalter(-100, 100);
+        //Writes values calculated from salting the cosine results from -100 to 100.
         cosineFormulaSalt.cosineWavesSalter(-100, 100);
     }
 }
