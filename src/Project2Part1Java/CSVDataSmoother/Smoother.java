3317a2984f3337c81282579212696f8e959bd465
CybErr404 <wattsm6@go.stockton.edu>
2024-04-23 14:45:56 -0400

    Worked on smoother and added MATLAB reports

--- src/Project2Part1Java/CSVDataSmoother/Smoother.java
+++ src/Project2Part1Java/CSVDataSmoother/Smoother.java
@@ -1,4 +1,58 @@
 package Project2Part1Java.CSVDataSmoother;
 
+import java.io.*;
+import java.util.ArrayList;
+
 public class Smoother {
+    public void smooth(String filename, int leftBound, int rightBound) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(filename));
+        ArrayList<Integer> saltedData = new ArrayList<>();
+        FileWriter fileWriter = new FileWriter("SmoothedPolynomialResults.csv");
+        BufferedWriter writer = new BufferedWriter(fileWriter);
+        String currentLine = "";
+        String[] values;
+        try {
+            while ((currentLine = reader.readLine()) != null) {
+                values = currentLine.split(",");
+                int value = Integer.parseInt(values[0]);
+                saltedData.add(value);
+            }
+
+            //0, 1, 2, 3, 4, 5, 6, 7, 8, 9,  10, 11
+            //1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
+            double sum = 0;
+            int count = 0;
+            for (int i = 0; i < saltedData.size(); i++) {
+                if (i >= leftBound || i <= rightBound) {
+                    sum = sum + saltedData.get(i);
+                    count++;
+                }
+//            else if(i > (saltedData.size() - bound)) {
+//
+//            }
+            }
+            double average = sum / count;
+            String smoothedValue = String.valueOf(average);
+            writer.write(smoothedValue);
+        }
+
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+		finally
+        {
+            try
+            {
+                if (reader != null) {
+                    reader.close();
+                    writer.close();
+                }
+            }
+            catch (IOException ex)
+            {
+                ex.printStackTrace();
+            }
+        }
+    }
 }
--- src/Project2Part1Java/CSVDataSmoother/TestSmoother.java
+++ src/Project2Part1Java/CSVDataSmoother/TestSmoother.java
@@ -1,7 +1,10 @@
 package Project2Part1Java.CSVDataSmoother;
 
-public class TestSmoother {
-    public static void main(String[] arg) {
+import java.io.IOException;
 
+public class TestSmoother {
+    public static void main(String[] arg) throws IOException {
+        Smoother test = new Smoother();
+        test.smooth("SinePlotterResults", 5000, 5000);
     }
 }
