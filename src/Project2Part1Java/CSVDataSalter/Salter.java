b9c0e9a02407f80bdde307c3f579698f63c05f22
CybErr404 <wattsm6@go.stockton.edu>
2024-04-24 22:58:45 -0400

    Fixed original salter

--- src/Project2Part1Java/CSVDataSalter/Salter.java
+++ src/Project2Part1Java/CSVDataSalter/Salter.java
@@ -1,9 +1,8 @@
 package Project2Part1Java.CSVDataSalter;
 
 //Import statement for FileNotFoundExceptions since the program deals with files.
-import java.io.FileNotFoundException;
+import java.io.*;
 //Import statement for the PrintWriter which writes information to the CSV file.
-import java.io.PrintWriter;
 //Import statement for the ArrayList class so values can be stored for future reference.
 import java.util.ArrayList;
 //Import statement for the Random class so random salt values can be generated.
@@ -27,21 +26,27 @@ public class Salter {
      * Salts the values calculated from the polynomial formula. If the index in the iteration
      * is even, the randomly generated salt value is added to the original value. If the index
      * of the iteration is odd, the randomly generated salt value is subtracted from the original
-     * value.
-     * @param start - starting increment value.
-     * @param end - ending increment value.
-     * @throws FileNotFoundException - a file is written to, so the exception was added just in case.
+     * value. Reads information from the plotter result file.
+     * @param file - file name that will be read.
+     * @throws IOException - in case the file to be read cannot be found.
      */
-    public void polynomialSalter(double start, double end) throws FileNotFoundException {
+    public void polynomialSalter(String file) throws IOException {
+        //Creates a new BufferedReader object that will read the plotter results file.
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        //Declares a variable that will hold the current line in the file.
+        String currentLine;
+        //While loop that continues until the next line is empty.
+        while((currentLine = reader.readLine()) != null) {
+            //Stores values found within the file into an array, splitting each by commas.
+            String[] values = currentLine.split(",");
+            //For loop that adds all values in the array to the result ArrayList.
+            for (String value : values) {
+                //Adds information to the result array.
+                resultArray.add(Double.valueOf(value));
+            }
+        }
         //Creates the PrintWriter object that will be used to write values to the CSV file.
         PrintWriter writer = new PrintWriter("PolynomialSalterResults.csv");
-        //Increments from the start to the end, calculating the value at each increment.
-        for(int i = (int) start; i <= end; i++) {
-            //Stores the result of the polynomial calculation.
-            double result = polynomialFormula(i);
-            //Adds the value into the result array.
-            resultArray.add(result);
-        }
         //Salts the values by randomly generating a number.
         for(int i = 0; i < resultArray.size(); i++) {
             //Creates a random number object.
@@ -69,37 +74,43 @@ public class Salter {
      * Salts the values calculated from the sine formula. If the index in the iteration
      * is even, the randomly generated salt value is added to the original value. If the index
      * of the iteration is odd, the randomly generated salt value is subtracted from the original
-     * value.
-     * @param start - starting increment value.
-     * @param end - ending increment value.
-     * @throws FileNotFoundException - a file is written to, so the exception was added just in case.
+     * value. Reads information from the plotter result file.
+     * @param file - file name that will be read.
+     * @throws IOException - in case the file to be read cannot be found.
      */
-    public void sineSalter(double start, double end) throws FileNotFoundException {
-        //Creates a PrintWriter object used to write values to the specified CSV file.
-        PrintWriter writer = new PrintWriter("SineSalterResults.csv");
-        //Increments through the start and end values to store all results in an array.
-        for(int i = (int) start; i <= end; i++) {
-            //Stores the result in a variable.
-            double result = sineFormula(i);
-            //Adds results to a result array.
-            resultArray.add(result);
+    public void sineSalter(String file) throws IOException {
+        //Creates a new BufferedReader object that will read the plotter results file.
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        //Declares a variable that will hold the current line in the file.
+        String currentLine;
+        //While loop that continues until the next line is empty.
+        while((currentLine = reader.readLine()) != null) {
+            //Stores values found within the file into an array, splitting each by commas.
+            String[] values = currentLine.split(",");
+            //For loop that adds all values in the array to the result ArrayList.
+            for (String value : values) {
+                //Adds information to the result array.
+                resultArray.add(Double.valueOf(value));
+            }
         }
-        //Salts the data.
+        //Creates the PrintWriter object that will be used to write values to the CSV file.
+        PrintWriter writer = new PrintWriter("SineSalterResults.csv");
+        //Salts the values by randomly generating a number.
         for(int i = 0; i < resultArray.size(); i++) {
             //Creates a random number object.
             Random randomNumber = new Random();
-            //Generates a random number to be used as the salt value.
+            //Generates a random number with which to salt the current value.
             int randomSaltValue = randomNumber.nextInt(200);
-            //If the index is even, add the random salt value to the original value.
+            //If the index is even, add the randomly generated salt value to the original value.
             if(i % 2 == 0) {
                 saltArray.add(resultArray.get(i) + randomSaltValue);
             }
-            //If the index is odd, subtract the random salt value from the original value.
+            //If the index is odd, subtract the randomly generated salt value from the original value.
             else {
                 saltArray.add(resultArray.get(i) - randomSaltValue);
             }
         }
-        //Writes the values to a CSV file.
+        //Writes the new, salted values to the file.
         for (Double number : saltArray) {
             writer.println(number);
         }
@@ -111,70 +122,47 @@ public class Salter {
      * Salts the values calculated from the cosine formula. If the index in the iteration
      * is even, the randomly generated salt value is added to the original value. If the index
      * of the iteration is odd, the randomly generated salt value is subtracted from the original
-     * value.
-     * @param start - starting increment value.
-     * @param end - ending increment value.
-     * @throws FileNotFoundException - a file is written to, so the exception was added just in case.
+     * value. Reads information from the plotter result file.
+     * @param file - file name that will be read.
+     * @throws IOException - in case the file to be read cannot be found.
      */
-    public void cosineWavesSalter(double start, double end) throws FileNotFoundException {
-        //Creates a PrintWriter object used to write values to the specified CSV file.
-        PrintWriter writer = new PrintWriter("CosineSalterResults.csv");
-        //Increments through the start and end values to store all results in an array.
-        for(int i = (int) start; i <= end; i++) {
-            //Stores the result in a variable.
-            double result = cosineWavesFormula(i);
-            //Adds results to a result array.
-            resultArray.add(result);
+    public void cosineWavesSalter(String file) throws IOException {
+        //Creates a new BufferedReader object that will read the plotter results file.
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        //Declares a variable that will hold the current line in the file.
+        String currentLine;
+        //While loop that continues until the next line is empty.
+        while((currentLine = reader.readLine()) != null) {
+            //Stores values found within the file into an array, splitting each by commas.
+            String[] values = currentLine.split(",");
+            //For loop that adds all values in the array to the result ArrayList.
+            for (String value : values) {
+                //Adds information to the result array.
+                resultArray.add(Double.valueOf(value));
+            }
         }
-        //Salts the data.
+        //Creates the PrintWriter object that will be used to write values to the CSV file.
+        PrintWriter writer = new PrintWriter("CosineSalterResults.csv");
+        //Salts the values by randomly generating a number.
         for(int i = 0; i < resultArray.size(); i++) {
             //Creates a random number object.
             Random randomNumber = new Random();
-            //Generates a random number to be used as the salt value.
+            //Generates a random number with which to salt the current value.
             int randomSaltValue = randomNumber.nextInt(200);
-            //If the index is even, add the random salt value to the original value.
+            //If the index is even, add the randomly generated salt value to the original value.
             if(i % 2 == 0) {
                 saltArray.add(resultArray.get(i) + randomSaltValue);
             }
-            //If the index is odd, subtract the random salt value from the original value.
+            //If the index is odd, subtract the randomly generated salt value from the original value.
             else {
                 saltArray.add(resultArray.get(i) - randomSaltValue);
             }
         }
-        //Writes the values to a CSV file.
+        //Writes the new, salted values to the file.
         for (Double number : saltArray) {
             writer.println(number);
         }
         //Closes the file to avoid leaks.
         writer.close();
-    }
-
-    /**
-     * This method calculates the result of inputting an x-value into 0.1x^2 - 0.5x - 2.
-     * @param increment - represents the x-value given to the equation.
-     * @return the result of calculating the equation with the given x-value.
-     */
-    public double polynomialFormula(double increment) {
-        return Math.pow((0.1 * increment), 2) - (0.5 * increment) - 2;
-    }
-
-    /**
-     * This method calculates the result of inputting an x-value into sin(2x).
-     * @param increment - represents the x-value given to the equation.
-     * @return the result of calculating the equation with the given x-value.
-     */
-    public double sineFormula(double increment) {
-        return Math.sin(2 * increment);
-    }
-
-    /**
-     * This method calculates the result of inputting an x-value into
-     * 3cos(x) - 5cos(2x) - 2cos(3x) - cos(4x).
-     * @param increment - represents the x-value given to the equation.
-     * @return the result of calculating the equation with the given x-value.
-     */
-    public double cosineWavesFormula(double increment) {
-        return (3 * Math.cos(increment)) - (5 * Math.cos(2 * increment))
-                - (2 * Math.cos(3 * increment)) - (Math.cos(4 * increment));
     }
 }
--- src/Project2Part1Java/CSVDataSalter/TestSalter.java
+++ src/Project2Part1Java/CSVDataSalter/TestSalter.java
@@ -2,6 +2,7 @@ package Project2Part1Java.CSVDataSalter;
 
 //Imports FileNotFoundExceptions to avoid issues with writing to files.
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 /**
  * @author Mia Watts
@@ -9,18 +10,18 @@ import java.io.FileNotFoundException;
  * the salting methods, running each formula from its start point, -100, to its end point, 100.
  */
 public class TestSalter {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         //Creates a salter object for the polynomial formula.
         Salter polynomialFormulaSalt = new Salter();
         //Creates a salter object for the sine formula.
         Salter sineFormulaSalt = new Salter();
         //Creates a salter object for the cosine formula.
         Salter cosineFormulaSalt = new Salter();
-        //Writes values calculated from salting the polynomial results from -100 to 100.
-        polynomialFormulaSalt.polynomialSalter(-100, 100);
-        //Writes values calculated from salting the sine results from -100 to 100.
-        sineFormulaSalt.sineSalter(-100, 100);
-        //Writes values calculated from salting the cosine results from -100 to 100.
-        cosineFormulaSalt.cosineWavesSalter(-100, 100);
+        //Writes values calculated from salting the polynomial results.
+        polynomialFormulaSalt.polynomialSalter("PolynomialPlotterResults.csv");
+        //Writes values calculated from salting the sine results.
+        sineFormulaSalt.sineSalter("SinePlotterResults.csv");
+        //Writes values calculated from salting the cosine results.
+        cosineFormulaSalt.cosineWavesSalter("CosinePlotterResults.csv");
     }
 }
